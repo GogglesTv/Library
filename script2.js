9c3449e7b3b6f6319fc47c75d4fd150309fb4721
Jalen Moore <jalenmarquise.moore@gmail.com>
2023-04-20 17:09:14 -0500

    Book card displays with the corresponding form information.

--- script2.js
+++ script2.js
@@ -8,7 +8,7 @@ let newRead = document.querySelector("#new-read");
 
 let myLibrary = [];
 
-function Book() {
+function Book(title, author, pages, read) {
   // the constructor...
   this.title = title;
   this.author = author;
@@ -16,6 +16,46 @@ function Book() {
   this.read = read;
 }
 
+function render() {
+  let libraryEl = document.querySelector(".my-books");
+  libraryEl.innerHTML = "";
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    let book = myLibrary[i];
+    let card = document.createElement("div");
+    card.classList.add("card", "book");
+    card.innerHTML = `<div class="card-body text-center">
+      <h5 class="card-title fw-bold mb-4" id="book-title">${book.title}</h5>
+      <h5 class="card-title fw-bold mb-4" id="book-author">${book.author}</h5>
+      <h5 class="card-title fw-bold" id="book-pages">${book.pages} Pages</h5>
+      <button class="btn w-100 my-3 fs-5 fw-bold" onclick="toggleRead(${i})" style="background-color: ${
+      book.read === "Read" ? "#81fea4" : "#f87171"
+    }">
+      ${book.read}</button>
+      <button class="btn w-100 fs-5 fw-bold" onclick="removeBook(${i})">Remove</button>
+    </div>`;
+    libraryEl.appendChild(card);
+  }
+}
+
 function addBookToLibrary() {
   // do stuff here
+  newRead.checked === true
+    ? (newRead.value = "Read")
+    : (newRead.value = "Not Read");
+
+  let newBook = new Book(
+    newTitle.value,
+    newAuthor.value,
+    newPages.value,
+    newRead.value
+  );
+  console.log(newBook);
+  myLibrary.push(newBook);
+  render();
 }
+
+submit.addEventListener("click", () => {
+  event.preventDefault();
+  addBookToLibrary();
+});
