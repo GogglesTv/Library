415346d7a4c72f7568efbce56921b21e87c23585
Jalen Moore <jalenmarquise.moore@gmail.com>
2023-04-20 16:48:34 -0500

    Selected the form inputs.

--- script2.js
+++ script2.js
@@ -1,7 +1,19 @@
+"use strict";
+
+const submit = document.querySelector("#submit");
+let newTitle = document.querySelector("#new-title");
+let newAuthor = document.querySelector("#new-author");
+let newPages = document.querySelector("#new-pages");
+let newRead = document.querySelector("#new-read");
+
 let myLibrary = [];
 
 function Book() {
   // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
